6392f19b3ca71ab093bbd9605b59138cb57c6973
Sam <sam@sammcnab.co.uk>
2024-01-01 15:45:06 +0000

    Fix Auto-Repo Link

--- .github/py_repo_tools/replace_repo_links.py
+++ .github/py_repo_tools/replace_repo_links.py
@@ -1,18 +1,32 @@
 # Python program to read
 # json file
-  
 import os
 import json
 import datetime
-  
+import subprocess
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Execute the git command to get the remote origin URL
+git_command = "git config --get remote.origin.url"
+git_url = subprocess.check_output(git_command, shell=True, text=True).strip()
+
+# Extract the repository name from the Git URL
+repository_name = git_url.split("/")[-1].replace(".git", "")
+
+# If the repository name is not found in the Git URL, use a default value
+New_Repo_Name = repository_name if repository_name else "Default_Repo_Name"
+
+# Format the repository name as "smcnab1/New_Repo_Name"
+Text_To_Replace_With = f"smcnab1/{New_Repo_Name}"
+
 # Opening JSON file
 f = open('.github/py_repo_tools/repo_config.json')
   
 # returns JSON object as 
 # a dictionary
 config = json.load(f)
-  
-New_Repo_Name = os.environ.get('New_Repo_Name', 'default_value')
 
 # Define the filename here you want to replace content in
 FileName = "README.md"
